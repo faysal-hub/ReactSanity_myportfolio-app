e59b64997b8d59d1cde7f49cf6a5f5300b7ee34a
faysalwzbm <ovitahsan@gmail.com>
2022-05-28 15:48:43 +0200

    import all the components into app component

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="app">
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
