dce551699ad39fea10d92a18ad7fac3321cd748d
faysalwzbm <ovitahsan@gmail.com>
2022-05-28 14:26:19 +0200

    add all the schemas

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,31 +1,30 @@
 export default {
-  // We name our schema
-  name: 'testimonials',
-  title: 'Testimonials',
-  type: 'document',
-  fields: [
-    {
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-    },
-    {
-      name: 'company',
-      title: 'Company',
-      type: 'string',
-    },
-    {
-      name: 'imageurl',
-      title: 'ImgURL',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: 'feedback',
-      title: 'Feedback',
-      type: 'string',
-    },
-  ],
-};
+    name:'testimonials',
+    title:'Testimonials',
+    type: 'document',
+    fields:[
+        { 
+            name:'name',
+            title:'Name',
+            type: 'string'
+        },
+        {
+            name:'company',
+            title:'Company',
+            type:'string'
+        },
+        {
+            name:'imgurl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },  
+        {
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
+        }
+    ]
+}
